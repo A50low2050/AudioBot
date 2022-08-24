d76c09e599e73983f0be4670a820826c06cdfbb8
Artem <artem.alshan@yandex.ru>
2022-08-24 11:26:34 +0300

    launch_bot.py

--- ArtAudio_bot/bot/launch_bot.py
+++ ArtAudio_bot/bot/launch_bot.py
@@ -1,12 +1,14 @@
-
+from create_bot import launch_bot
 from aiogram import executor
 from create_bot import dp
-from HandlersBot import start, audio_ytb_convert, watch_settings
+from HandlersBot import start, audio_ytb_convert, watch_settings, download_audio
 
+launch_bot()
 
 start.register_handler_start(dp)
 audio_ytb_convert.register_handlers_audio_ytb(dp)
 watch_settings.register_handlers_watch_settings(dp)
+download_audio.register_handlers_download_audio(dp)
 
 
 executor.start_polling(dp, skip_updates=True)
